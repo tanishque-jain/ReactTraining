2c43355291e2f496fe6d9c2ae57b79c84689fdf4
Tanishque <tanishque.jain@appinventiv.com>
2021-06-02 17:16:45 +0530

    added css to home

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import './index.css'
 
 class Home extends Component {
     render(){
         return (
-            <div>
-                <h1>{"Hello from Home"}</h1>
-            </div>
+            // <div>
+            //     <h1>{"Hello from Home"}</h1>
+            // </div>
+                      <div className={"homeContainer"}>
+                      <h1 className={"h1tag"}>{"Hello from Home"}</h1>
+                      <h2 id={"h2tag"}>{"Hello from Home"}</h2>
+                      <h3>{"Hello from Home"}</h3>
+                    </div>
         );
     }
 }
